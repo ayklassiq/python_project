e1aa4080ac9197513b79a0d8753061fbe5039b5c
ayklassiq <49102713+ayklassiq@users.noreply.github.com>
2020-07-02 10:09:03 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+
+	name =  'password-management',
+	version = '1.0.0',
+	py_modules = ['password-management'],
+	author = 'Ayoade Adesope',
+	author_email = 'auadesope@gmail.com',
+	url = '',
+	description = 'A simple program that allows a user to manage passwords and contains a password generator',
+
+
+
+	)
